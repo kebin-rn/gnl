808b3e94dee9c3b493b080d1f987f87729c3fe0c
rnihei a.k.a. Kebin <42.kebin-cf.net>
2025-07-22 15:03:27 +0900

    i think they are fixed

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: rnihei <rnihei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 15:47:05 by rnihei            #+#    #+#             */
-/*   Updated: 2025/07/22 13:15:41 by rnihei           ###   ########.fr       */
+/*   Updated: 2025/07/22 14:54:55 by rnihei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,10 +59,9 @@ char	*ft_strjoin(char *s1, char *s2)
 	if (!s1)
 	{
 		s1 = (char *)malloc(1 * sizeof(char));
-		if (!s1)
-			return (NULL);
-		s1[0] = '\0';
 	}
+	if (!s1)
+		return (NULL);
 	if (!s1 || !s2)
 		return (NULL);
 	new_str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
---
+++ test.c
@@ -0,0 +1,29 @@
+#include "./get_next_line.h"
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+	int		flag;
+
+	fd = open("./test.txt", O_RDONLY);
+	flag = 3;
+	while (flag)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			return (0);
+		printf("%s", line);
+		free(line);
+		flag--;
+	}
+	while ((line = get_next_line(fd)) != NULL)
+		free(line);
+	return (0);
+}
